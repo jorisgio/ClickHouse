9c6de8193a65892f352a8f7a71e17e22589e833c
Joris Giovannangeli <joris.giovannangeli@ahrefs.com>
2020-10-26 10:06:56 +0100

    zstd: add long range option for compression

--- src/Compression/CompressionCodecZSTD.cpp
+++ src/Compression/CompressionCodecZSTD.cpp
@@ -1,17 +1,16 @@
-#include <Compression/CompressionCodecZSTD.h>
-#include <Compression/CompressionInfo.h>
-#include <Compression/CompressionFactory.h>
 #include <zstd.h>
-#include <Parsers/IAST.h>
-#include <Parsers/ASTLiteral.h>
-#include <Parsers/ASTFunction.h>
-#include <Common/typeid_cast.h>
+#include <Compression/CompressionCodecZSTD.h>
+#include <Compression/CompressionFactory.h>
+#include <Compression/CompressionInfo.h>
 #include <IO/WriteHelpers.h>
+#include <Parsers/ASTFunction.h>
+#include <Parsers/ASTLiteral.h>
+#include <Parsers/IAST.h>
+#include <Common/typeid_cast.h>
 
 
 namespace DB
 {
-
 namespace ErrorCodes
 {
     extern const int CANNOT_COMPRESS;
@@ -29,7 +28,15 @@ uint8_t CompressionCodecZSTD::getMethodByte() const
 ASTPtr CompressionCodecZSTD::getCodecDesc() const
 {
     auto literal = std::make_shared<ASTLiteral>(static_cast<UInt64>(level));
-    return makeASTFunction("ZSTD", literal);
+    if (enable_long_range)
+    {
+        auto literal_window = std::make_shared<ASTLiteral>(static_cast<UInt64>(window_log));
+        return makeASTFunction("ZSTD", literal, literal_window);
+    }
+    else
+    {
+        return makeASTFunction("ZSTD", literal);
+    }
 }
 
 UInt32 CompressionCodecZSTD::getMaxCompressedDataSize(UInt32 uncompressed_size) const
@@ -40,7 +47,15 @@ UInt32 CompressionCodecZSTD::getMaxCompressedDataSize(UInt32 uncompressed_size) 
 
 UInt32 CompressionCodecZSTD::doCompressData(const char * source, UInt32 source_size, char * dest) const
 {
-    size_t compressed_size = ZSTD_compress(dest, ZSTD_compressBound(source_size), source, source_size, level);
+    ZSTD_CCtx * cctx = ZSTD_createCCtx();
+    ZSTD_CCtx_setParameter(cctx, ZSTD_c_compressionLevel, level);
+    if (enable_long_range)
+    {
+        ZSTD_CCtx_setParameter(cctx, ZSTD_c_enableLongDistanceMatching, 1);
+        ZSTD_CCtx_setParameter(cctx, ZSTD_c_windowLog, window_log);
+    }
+    size_t compressed_size = ZSTD_compress2(cctx, dest, ZSTD_compressBound(source_size), source, source_size);
+    ZSTD_freeCCtx(cctx);
 
     if (ZSTD_isError(compressed_size))
         throw Exception("Cannot compress block with ZSTD: " + std::string(ZSTD_getErrorName(compressed_size)), ErrorCodes::CANNOT_COMPRESS);
@@ -57,21 +72,25 @@ void CompressionCodecZSTD::doDecompressData(const char * source, UInt32 source_s
         throw Exception("Cannot ZSTD_decompress: " + std::string(ZSTD_getErrorName(res)), ErrorCodes::CANNOT_DECOMPRESS);
 }
 
-CompressionCodecZSTD::CompressionCodecZSTD(int level_)
-    : level(level_)
+CompressionCodecZSTD::CompressionCodecZSTD(int level_, int window_log_) : level(level_), enable_long_range(true), window_log(window_log_)
+{
+}
+
+CompressionCodecZSTD::CompressionCodecZSTD(int level_) : level(level_), enable_long_range(false), window_log(0)
 {
 }
 
 void registerCodecZSTD(CompressionCodecFactory & factory)
 {
     UInt8 method_code = UInt8(CompressionMethodByte::ZSTD);
-    factory.registerCompressionCodec("ZSTD", method_code, [&](const ASTPtr & arguments) -> CompressionCodecPtr
-    {
+    factory.registerCompressionCodec("ZSTD", method_code, [&](const ASTPtr & arguments) -> CompressionCodecPtr {
         int level = CompressionCodecZSTD::ZSTD_DEFAULT_LEVEL;
         if (arguments && !arguments->children.empty())
         {
-            if (arguments->children.size() > 1)
-                throw Exception("ZSTD codec must have 1 parameter, given " + std::to_string(arguments->children.size()), ErrorCodes::ILLEGAL_SYNTAX_FOR_CODEC_TYPE);
+            if (arguments->children.size() > 2)
+                throw Exception(
+                    "ZSTD codec must have 1 or 2 parameters, given " + std::to_string(arguments->children.size()),
+                    ErrorCodes::ILLEGAL_SYNTAX_FOR_CODEC_TYPE);
 
             const auto children = arguments->children;
             const auto * literal = children[0]->as<ASTLiteral>();
@@ -80,9 +99,31 @@ void registerCodecZSTD(CompressionCodecFactory & factory)
 
             level = literal->value.safeGet<UInt64>();
             if (level > ZSTD_maxCLevel())
-                throw Exception("ZSTD codec can't have level more that " + toString(ZSTD_maxCLevel()) + ", given " + toString(level), ErrorCodes::ILLEGAL_CODEC_PARAMETER);
-        }
+                throw Exception(
+                    "ZSTD codec can't have level more that " + toString(ZSTD_maxCLevel()) + ", given " + toString(level),
+                    ErrorCodes::ILLEGAL_CODEC_PARAMETER);
+            if (arguments->children.size() > 1)
+            {
+                const auto * window_literal = children[1]->as<ASTLiteral>();
+                if (!window_literal)
+                    throw Exception("ZSTD codec second argument must be integer", ErrorCodes::ILLEGAL_CODEC_PARAMETER);
 
+                const int window_log = window_literal->value.safeGet<UInt64>();
+
+                ZSTD_bounds window_log_bounds = ZSTD_cParam_getBounds(ZSTD_c_windowLog);
+                if (ZSTD_isError(window_log_bounds.error))
+                    throw Exception(
+                        "ZSTD windowLog paramater is not supported " + std::string(ZSTD_getErrorName(window_log_bounds.error)),
+                        ErrorCodes::ILLEGAL_CODEC_PARAMETER);
+                if (window_log != 0 && (window_log > window_log_bounds.upperBound || window_log < window_log_bounds.lowerBound))
+                    throw Exception(
+                        "ZSTD codec can't have window log more that " + toString(window_log_bounds.upperBound) + " and lower than "
+                            + toString(window_log_bounds.lowerBound) + ", given " + toString(window_log),
+                        ErrorCodes::ILLEGAL_CODEC_PARAMETER);
+
+                return std::make_shared<CompressionCodecZSTD>(level, window_log);
+            }
+        }
         return std::make_shared<CompressionCodecZSTD>(level);
     });
 }
